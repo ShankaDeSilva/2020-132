2c45d0aca5c1c44b03179a97ce9dce23bd626f01
IT17095518 <it17095518@my.sliit.lk>
2020-05-16 23:43:17 +0530

    Create MammalDetailsController.cs

---
+++ MammalDetailsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Basic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MammalDetailsController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { " value1", "value2" };
+        }
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public void Post(int id, [FromBody]string value)
+        {
+        }
+
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
