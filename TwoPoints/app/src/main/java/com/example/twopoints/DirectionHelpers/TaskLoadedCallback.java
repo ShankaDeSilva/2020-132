2c30e0a82f479874fe37da6bf16a9bd40f2af474
Shanka_IT17025690 <it17025690@my.sliit.lk>
2020-07-03 18:18:52 +0530

    Draw path between two locations
    Draw a polyline between two locations on a google map

---
+++ TwoPoints/app/src/main/java/com/example/twopoints/DirectionHelpers/TaskLoadedCallback.java
@@ -0,0 +1,5 @@
+package com.example.twopoints.DirectionHelpers;
+
+public interface TaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
---
+++ TwoPoints/app/src/main/java/com/example/twopoints/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.twopoints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+    private MarkerOptions place1, place2;
+    Button getDirection;
+    private Polyline currentPolyline;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
