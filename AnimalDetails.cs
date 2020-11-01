8a52d620dca184bfce529a0c0b20ba98f78e39c8
De Silva W.N.K <it17095518@my.sliit.lk>
2020-11-01 23:10:56 +0530

    Animal Details Model

---
+++ AnimalDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_project.Models
+{
+    public class AnimalDetails
+    {
+        public int Id { get; set; }
+        public int AnimalId { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
