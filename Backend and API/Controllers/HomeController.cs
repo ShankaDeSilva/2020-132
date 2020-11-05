6c1ea21324127ec410ee1af068a54c04e0098a4e
De Silva W.N.K <it17095518@my.sliit.lk>
2020-11-06 01:18:23 +0530

    Home Controller

---
+++ Backend and API/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using web_project.Models;
+
+namespace web_project.Controllers
+{
+  
+    public class HomeController : Controller
+    {
+       
+        public HomeController(  )
+        {
+
+          
+
+        }
+
+       
+        public IActionResult Index()
+        {
+            string a = AppManage.root + "/Analyze.html";
+
+
+            var result = new PhysicalFileResult(a, "text/html");
+            return result;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ViewLogIn()
+        {
+            AppManage.LoggedInType = 0;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
